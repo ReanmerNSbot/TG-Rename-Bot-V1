c3f0ec12234d3bde459a435868be2f534770dce3
niya369 <71867099+niya369@users.noreply.github.com>
2020-11-06 13:38:08 +0530

    Create help_text.py

--- plugins/help_text.py
+++ plugins/help_text.py
@@ -70,13 +70,10 @@ async def start(bot, update):
         reply_markup=InlineKeyboardMarkup(
             [
                 [
-                    InlineKeyboardButton('Support Channel', url='https://t.me/Mai_bOTs'),
-                    InlineKeyboardButton('Feedback', url='https://t.me/No_OnE_Kn0wS_Me')
+                    InlineKeyboardButton('Support Group', url='https://t.me/TrollJunction'),
+                    InlineKeyboardButton('Feedback', url='https://t.me/Iam_The_Master')
                 ],
-                [
-                    InlineKeyboardButton('Other Bots', url='https://t.me/Mai_bOTs/17'),
-                    InlineKeyboardButton('Source', url='https://github.com/No-OnE-Kn0wS-Me/FileRenameBot')
-                ]
+
             ]
         ),
         reply_to_message_id=update.message_id
