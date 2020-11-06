1e2144f0e314c4be5c2baad8f4d34f406fef41f7
niya369 <71867099+niya369@users.noreply.github.com>
2020-11-06 13:53:37 +0530

    Create translation.py

--- translation.py
+++ translation.py
@@ -33,5 +33,5 @@ class Translation(object):
 1.🔹 <b>Send me any Telegram File.</b> \n
 2.🔹 <b>Reply to that message with /rename New file name.extnsion</b> \n
 3.🔹 <b> Send A Photo to make it as permanent Thumbnail</b> \n
-4.🔹 <b> I Can Also Convert Files Into Video through /c2v Command </b>   \n \n <b>Made With Love By: @mai_boTs</b>"""
+4.🔹 <b> I Can Also Convert Files Into Video through /c2v Command </b>   \n \n <b>Made With Love By: @Iam_The_Master 🔥</b>"""
 
